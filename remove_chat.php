6a850bca63b26c96592876a7547b0e32638f7ca0
daw2-casado20 <55798413+daw2-casado20@users.noreply.github.com>
2020-04-08 18:01:32 +0200

    Update remove_chat.php

--- remove_chat.php
+++ remove_chat.php
@@ -3,18 +3,24 @@
 //remove_chat.php
 
 include('database_connection.php');
+include('class.login.php');
+include('class.login.details.php');
+include('class.chat_message.php');
 
 if(isset($_POST["chat_message_id"]))
 {
-	$query = "
+	$nuevomensaje3 = new Chat_Message($connect, $_POST["chat_message_id"]);
+	$nuevomensaje3->setstatus(2);
+	$nuevomensaje3->Save();
+	/*$query = "
 	UPDATE chat_message 
 	SET status = '2' 
-	WHERE chat_message_id = '".$_POST["chat_message_id"]."'
+	WHERE id = '".$_POST["chat_message_id"]."'
 	";
 
 	$statement = $connect->prepare($query);
 
-	$statement->execute();
+	$statement->execute();*/
 }
 
 ?>
