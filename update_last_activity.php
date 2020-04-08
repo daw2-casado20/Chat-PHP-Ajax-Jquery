c31a46befdb561c68c061b9d553cf1012b228663
daw2-casado20 <55798413+daw2-casado20@users.noreply.github.com>
2020-04-08 18:02:05 +0200

    Update update_last_activity.php

--- update_last_activity.php
+++ update_last_activity.php
@@ -3,18 +3,25 @@
 //update_last_activity.php
 
 include('database_connection.php');
+include('class.login.php');
+include('class.login.details.php');
+include('class.chat_message.php');
 
 session_start();
 
-$query = "
+$logindet2 = new Login_Details($connect, $_SESSION["login_details_id"]);
+$logindet2->setlast_activity(now());
+$logindet2->Save();
+
+/*$query = "
 UPDATE login_details 
 SET last_activity = now() 
-WHERE login_details_id = '".$_SESSION["login_details_id"]."'
+WHERE id = '".$_SESSION["login_details_id"]."'
 ";
 
 $statement = $connect->prepare($query);
 
-$statement->execute();
+$statement->execute();*/
 
 ?>
 
