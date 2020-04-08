ee78fadbc90f7cb4a9ad408e21349d8b42f48d58
daw2-casado20 <55798413+daw2-casado20@users.noreply.github.com>
2020-04-08 17:31:48 +0200

    Create class.login.php

---
+++ class.login.php
@@ -0,0 +1,33 @@
+<?php
+class Login extends DataBoundObject {
+
+        protected $username;
+        protected $password;        
+        
+
+        protected function DefineTableName() {
+                return("login");
+        }
+
+        protected function DefineRelationMap() {
+                return(array(
+                        "id" => "ID",
+                        "username" => "username",
+                        "password" => "password",
+                ));
+        }
+        public function getData($connect){
+            $query = "
+               SELECT * FROM login 
+                WHERE username = :username
+             ";
+             $statment = $connect->prepare($query);
+             $statment->execute(
+                array(
+                  ':username' => $this->username
+                 )
+              );
+            return $statment;
+        }
+}
+?>
